7239baa6e009935f29b2fd81f537828d95ffd54c
Rahulkrishnan R <rahulkrish.eee@gmail.com>
2024-05-14 14:51:41 +0530

    Update App.js
    removed extra texts and added spacing

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
     <div className="App">
       <h1>Hi I am Rahul</h1>
       <h2>I am a Full Stack Web Developer </h2>
-      <h3>Here is my <a href="https://github.com/rahulkrish57">Github</a>link </h3>
-      <h3>Here is my Email <a href="rahulkrishdev@gmail.com">rahulkrishdev@gmail.com</a>link </h3>
+      <h3>Here is my <a href="https://github.com/rahulkrish57"> Github</a>link </h3>
+      <h3>Here is my Email <a href="rahulkrishdev@gmail.com">rahulkrishdev@gmail.com</a></h3>
     </div>
   );
 }
