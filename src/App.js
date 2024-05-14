de003eb7524e53c86d699e968cadc096732f4c9a
Rahulkrishnan R <rahulkrish.eee@gmail.com>
2024-05-14 14:41:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hi I am Rahul</h1>
+      <h2>I am a Full Stack Web Developer </h2>
+    </div>
+  );
+}
